7140badc1171a3ec5e0ba8cf8289d0d8f952a4f4
Razwanul <mdrazwanulislam014@gmail.com>
2026-08-25 19:05:13 +0600

    Add mime type & file limit

--- server.js
+++ server.js
@@ -5,33 +5,42 @@ import multer from "multer";
 const app = express();
 app.use(express.json());
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "uploads/",
+    limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+            cb(null, true);
+        } else {
+            cb(new Error("Only JPEG and PNG files are allowed."), false);
+        }
+    },
+ });
 
-// app.post("/upload", upload.single("image"), (req, res) => {
-//   if (!req.file) {
-//     return res.status(400).send("No file uploaded.");
-//   }
-//   res.json({ message: "File uploaded successfully.", file: req.file });
-// });
-
-app.post("/profile", upload.fields([
-    {name: "thumbnail", maxCount: 1},
-    {name: "gallery", maxCount: 5}
-]),(req, res) => {
-  if (!req.files || req.files.length === 0) {
-    return res.status(400).send("No files uploaded.");
+app.post("/profile", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
   }
-  console.log(req.body.name);
-  console.log(req.body.email);
-  console.log(req.files.thumbnail.length+req.files.gallery.length);
+  res.json({ message: "File uploaded successfully.", file: req.file });
+});
+
+// app.post("/profile", upload.fields([
+//     {name: "thumbnail", maxCount: 1},
+//     {name: "gallery", maxCount: 5}
+// ]),(req, res) => {
+//   if (!req.files || req.files.length === 0) {
+//     return res.status(400).send("No files uploaded.");
+//   }
+//   console.log(req.body.name);
+//   console.log(req.body.email);
+//   console.log(req.files.thumbnail.length+req.files.gallery.length);
 
 
-  res.json({
-    name: req.body.name,
-    email: req.body.email,
-    thumbnail: req.files.gallery.map((file) => file.filename),
-    gallery: req.files.gallery.map((file) => file.filename),
-  })});
+//   res.json({
+//     name: req.body.name,
+//     email: req.body.email,
+//     thumbnail: req.files.gallery.map((file) => file.filename),
+//     gallery: req.files.gallery.map((file) => file.filename),
+//   })});
 
 const PORT = 3000;
 
