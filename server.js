04075768dd3d28abf914df6a5affdb09dd90bd77
Razwanul <mdrazwanulislam014@gmail.com>
2026-08-25 15:51:55 +0600

    add multiple field like text

--- server.js
+++ server.js
@@ -3,15 +3,30 @@ import multer from "multer";
 
 
 const app = express();
+app.use(express.json());
 
 const upload = multer({ dest: "uploads/" });
 
-app.post("/upload", upload.single("image"), (req, res) => {
+// app.post("/upload", upload.single("image"), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).send("No file uploaded.");
+//   }
+//   res.json({ message: "File uploaded successfully.", file: req.file });
+// });
+
+app.post("/profile", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
-  res.json({ message: "File uploaded successfully.", file: req.file });
-});
+  console.log(req.body.name);
+  console.log(req.body.email);
+  console.log(req.file);
+
+  res.json({
+    name: req.body.name,
+    email: req.body.email,
+    file: req.file.filename,
+  })});
 
 const PORT =3000;
 
