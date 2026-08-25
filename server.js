e6613e2f4825117c3a7e1b470a3791adc5dee896
Razwanul <mdrazwanulislam014@gmail.com>
2026-08-25 16:24:45 +0600

    sent multiple file in one request using array

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ const upload = multer({ dest: "uploads/" });
 //   res.json({ message: "File uploaded successfully.", file: req.file });
 // });
 
-app.post("/profile", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send("No file uploaded.");
+app.post("/profile", upload.array("images", 10), (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).send("No files uploaded.");
   }
   console.log(req.body.name);
   console.log(req.body.email);
-  console.log(req.file);
+  console.log(req.files.length);
 
   res.json({
     name: req.body.name,
     email: req.body.email,
-    file: req.file.filename,
+    files: req.files.map((file) => file.filename),
   })});
 
-const PORT =3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
