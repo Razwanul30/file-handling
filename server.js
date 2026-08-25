e665255995aa393f0762cad4f3d89420c6d349cb
Razwanul <mdrazwanulislam014@gmail.com>
2026-08-25 16:47:45 +0600

    appli fields to send file separately

--- server.js
+++ server.js
@@ -14,18 +14,23 @@ const upload = multer({ dest: "uploads/" });
 //   res.json({ message: "File uploaded successfully.", file: req.file });
 // });
 
-app.post("/profile", upload.array("images", 10), (req, res) => {
+app.post("/profile", upload.fields([
+    {name: "thumbnail", maxCount: 1},
+    {name: "gallery", maxCount: 5}
+]),(req, res) => {
   if (!req.files || req.files.length === 0) {
     return res.status(400).send("No files uploaded.");
   }
   console.log(req.body.name);
   console.log(req.body.email);
-  console.log(req.files.length);
+  console.log(req.files.thumbnail.length+req.files.gallery.length);
+
 
   res.json({
     name: req.body.name,
     email: req.body.email,
-    files: req.files.map((file) => file.filename),
+    thumbnail: req.files.gallery.map((file) => file.filename),
+    gallery: req.files.gallery.map((file) => file.filename),
   })});
 
 const PORT = 3000;
